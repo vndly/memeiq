d1656505c3e7186283fabe4661b06f859a0d62f4
Mauricio Togneri <mauricio.togneri@gmail.com>
2026-09-13 14:51:01 +0200

    Improvements

--- frontend/src/components/screens/blank_screen.vue
+++ frontend/src/components/screens/blank_screen.vue
@@ -1,13 +1,33 @@
 <script setup lang="ts">
+import faviconUrl from '@/assets/favicon.png'
 </script>
 
 <template>
-  <main class="blank-screen"></main>
+  <main class="blank-screen">
+    <img
+      :src="faviconUrl"
+      alt="Meme IQ logo"
+      class="logo"
+      width="128"
+      height="128"
+    >
+  </main>
 </template>
 
 <style scoped>
 .blank-screen {
+  display: grid;
+  place-items: center;
   min-height: 100%;
   width: 100%;
+  padding: 1.5rem;
+}
+
+.logo {
+  display: block;
+  width: 128px;
+  height: 128px;
+  max-width: 100%;
+  user-select: none;
 }
 </style>
